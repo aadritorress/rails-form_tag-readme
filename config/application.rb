ed75298c5361cd2af004a48c344a414fdc2ed3e9
Adriana <adriana.amaral@msn.com>
2021-03-20 17:09:48 -0500

    commit tree f7675231c342449c5d34b6eb5599e1583b251501

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+class Application < Rails::Application
+  config.web_console.whitelisted_ips = '<IP address here>'
+end
 
 module BlogFlash
   class Application < Rails::Application
